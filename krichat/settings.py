7dfd0358644790e1bf1031fa6bd42000c4f1a97e
Denis <email@mail.com>
2017-04-08 22:45:47 +0300

    Style auth

--- krichat/settings.py
+++ krichat/settings.py
@@ -82,9 +82,9 @@ DATABASES = {
     #}
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'CHANGE THIS',                      # Or path to database file if using sqlite3.
-        'USER': 'CHANGE THIS',                      # Not used with sqlite3.
-        'PASSWORD': 'CHANGE THIS',                  # Not used with sqlite3.
+        'NAME': 'chat',                      # Or path to database file if using sqlite3.
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
