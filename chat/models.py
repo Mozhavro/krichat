0a7a0d64fe8c9583fc9ef3a585aa43c882f87170
Mozhavro <mozhario@gmail.com>
2018-01-05 13:02:41 +0200

    PEP8 fixes

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Chatter(models.Model):
-    user 		= models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     logout_date = models.DateTimeField()
 
+
 class Room(models.Model):
-	title 		= models.CharField(max_length=100)
-	chatters 	= models.ManyToManyField(User)
+    title = models.CharField(max_length=100)
+    chatters = models.ManyToManyField(User)
 
 class Message(models.Model):
-	sender 		= models.ForeignKey(User)
-	room 		= models.ForeignKey('chat.Room')
-	text 		= models.CharField(max_length=1000)
-	is_loud		= models.BooleanField(default=True)
-	date 		= models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(User)
+    room = models.ForeignKey('chat.Room')
+    text = models.CharField(max_length=1000)
+    is_loud = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
--- chat/urls.py
+++ chat/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-
-
-	url('', include('pwa.urls')),
+    url('', include('pwa.urls')),
 
     url(r'^$', views.chat_room, name='index'),
 
