fee8858d868f74ae32c5e0963591c5b877caf6db
Mozhavro <mozhario@gmail.com>
2018-01-05 14:27:07 +0200

    Refactored urls

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,5 @@
 from . import views
 from django.conf.urls import include, url
-from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url('', include('pwa.urls')),
@@ -9,10 +7,4 @@ urlpatterns = [
     url(r'^$', views.chat_room, name='index'),
 
     url(r'^(?P<slug>[-\w]+)/$', views.chat_room),
-
-    #auth
-    url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
-    url(r'^admin/', admin.site.urls),
 ]
